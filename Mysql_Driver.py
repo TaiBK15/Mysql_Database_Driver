ffc41b82d51284aa073c6ca57cb3363dbdcacbc8
TaiBK15 <tainguyentp2505@gmail.com>
2019-03-09 12:44:14 +0700

    Update code for MySQL driver with version 1.1.0

--- Mysql_Driver.py
+++ Mysql_Driver.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 
+
 class MysqlQuery:
 
     def __init__(self, host, database, user, password):
@@ -10,11 +11,6 @@ class MysqlQuery:
         self.password = password
         self.connect = None
         self.cursor = None
-
-    def deleteTable(self, device_tab):
-        sql_drop_tab_query = "DROP TABLE " + device_tab
-        self.cursor.execute(sql_drop_tab_query)
-        print("Delete table " + device_tab + " successfully!\n")
 
     def connectDatabase(self):
         try:
@@ -27,33 +23,82 @@ class MysqlQuery:
                 print("Connected to MySQL database")
         except mysql.connector.Error as error:
             print("Failed to connect to database!")
+            print(error)
+
             
+    def queryCmd(self, sql_query):
+        try:
+            self.cursor.execute(sql_query)
+            self.connect.commit()
+            print("Query OK!")
+        except mysql.connector.Error as error:
+            print("Query failed!")
+            print(error)
+
+    def createTable(self, device_tab):
+        try:
+            sql_create_table = "CREATE TABLE " + device_tab + "(ID INT NOT NULL AUTO_INCREMENT,\
+                                                            TIME TIMESTAMP NOT NULL,\
+                                                            DATA VARCHAR(255) NOT NULL,\
+                                                            PRIMARY KEY (ID));"
+            self.cursor.execute(sql_create_table)
+        except mysql.connector.Error as error:
+            print("Failed to create table!")
+            print(error)
+
+
+    def deleteTable(self, device_tab):
+        try:
+            sql_drop_tab_query = "DROP TABLE " + device_tab + ";"
+            self.cursor.execute(sql_drop_tab_query)
+            print("Delete table " + device_tab + " successfully!")
+        except mysql.connector.Error as error:
+            print("Failed to delete table!")
+            print(error)
+
+
     def insertTable(self,device_tab, data):
-        sql_insert_query = "INSERT INTO "+ device_tab + "(DATA) VALUES('" + data + "')"
-        self.cursor.execute(sql_insert_query)
-        self.connect.commit()
-        print("Insert into database successfully!")
+        try:
+            sql_insert_query = "INSERT INTO "+ device_tab + "(DATA) VALUES('" + data + "')"
+            self.cursor.execute(sql_insert_query)
+            self.connect.commit()
+            print("Insert into database successfully!")
+        except mysql.connector.Error as error:
+            print("Failed to insert ino table!")
+            print(error)
 
     def readAllTable(self, device_tab):
-        sql_read_all_query = "SELECT * FROM "+ device_tab + ";"
-        self.cursor.execute(sql_read_all_query)
-        records = self.cursor.fetchall()
-        for row in records:
-            print(row)
+        try:
+            sql_read_all_query = "SELECT * FROM "+ device_tab + ";"
+            self.cursor.execute(sql_read_all_query)
+            records = self.cursor.fetchall()
+            for row in records:
+                print(row)
+        except mysql.connector.Error as error:
+            print("Failed to read data!")
+            print(error)
 
     def readRecentTable(self, device_tab, size_row):
-        sql_read_recent_query = "SELECT * FROM "+ device_tab + " ORDER BY TIME DESC;"
-        self.cursor.execute(sql_read_recent_query)
-        records = self.cursor.fetchmany(size_row)
-        for row in records:
-            print(row)
+        try:
+            sql_read_recent_query = "SELECT * FROM "+ device_tab + " ORDER BY TIME DESC;"
+            self.cursor.execute(sql_read_recent_query)
+            records = self.cursor.fetchmany(size_row)
+            for row in records:
+                print(row)
+        except mysql.connector.Error as error:
+            print("Failed to read data!")
+            print(error)
 
-    def deleteDataTable(self):
-        sql_delete_query = "TRUNCATE mqtt_data;"
-        self.cursor.execute(sql_delete_query)
-        self.connect.commit()
-        print("Delete database successfully!\n")
- 
+    def deleteDataTable(self, device_tab):
+        try:
+            sql_delete_query = "TRUNCATE " + device_tab + ";"
+            self.cursor.execute(sql_delete_query)
+            self.connect.commit()
+            print("Delete database successfully!")
+        except mysql.connector.Error as error:
+            print("Failed to delete data!")
+            print(error)
+
     def finishConnect(self):
         if(self.connect.is_connected()):
               self.cursor.close()
--- main.py
+++ main.py
@@ -1,12 +1,112 @@
 from Mysql_Driver import MysqlQuery
 
+try:
+    import paho.mqtt.client as mqtt
+except ImportError:
+
+    import os
+    import inspect
+    cmd_subfolder = os.path.realpath(
+        os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], "../src")))
+    if cmd_subfolder not in sys.path:
+        sys.path.insert(0, cmd_subfolder)
+
+    import paho.mqtt.client as mqtt
+    print "import error"
+
+
+# Define event callbacks
+
+def on_connect(mosq, obj, rc):
+    """
+    Connect MQTT Server
+    :param mosq:
+    :param obj:
+    :param rc:
+    :return:
+    """
+    print ("on_connect:: Connected with result code " + str(rc))
+    print("rc: " + str(rc))
+
+
+def on_message(mosq, obj, msg):
+    """
+    Message from MQTT Server
+    :param mosq:
+    :param obj:
+    :param msg:
+    :return:
+    """
+    print ("on_message:: this means  I got a message from broker for this topic")
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    mysqlQuery = MysqlQuery(HOST, DATABASE, USER, PASSWORD)
+    mysqlQuery.connectDatabase()
+    # mysqlQuery.deleteDataTable("device_1")
+
+    mysqlQuery.insertTable("device_1", str(msg.payload))
+    mysqlQuery.finishConnect()
+
+    # Send payload to C process
+    # conn.sendall(msg.payload)
+
+
+def on_publish(mosq, obj, mid):
+    """
+    Show message ID when publishing to MQTT Server
+    :param mosq:
+    :param obj:
+    :param mid:
+    :return:
+    """
+    print("publish to cloudmqtt " + str(mid))
+
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    """
+    :param mosq:
+    :param obj:
+    :param mid:
+    :param granted_qos:
+    :return:
+    """
+    print("This means broker has acknowledged my subscribe request")
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(mosq, obj, level, string):
+    """
+    :param mosq:
+    :param obj:
+    :param level:
+    :param string:
+    :return:
+    """
+    print(string)
+
+
 HOST = 'localhost'
 DATABASE = 'MQTT_DATABASE'
 USER = 'root'
 PASSWORD = '123456'
 
+
+client = mqtt.Client()
+# Assign event callbacks
+client.on_message = on_message
+client.on_connect = on_connect
+client.on_publish = on_publish
+client.on_subscribe = on_subscribe
+
+# Set up password and IP
+client.username_pw_set("local_broker", "123456")
+client.connect('localhost', 1883)
+
+client.subscribe("local/test", 0)
+client.loop_forever()
+
+
 #Create object via contructor method __init__
-mysqlQuery = MysqlQuery(HOST, DATABASE, USER, PASSWORD)
+# mysqlQuery = MysqlQuery(HOST, DATABASE, USER, PASSWORD)
 
 """Step to implement QUERY command to MySQL database
 	1. Create MySQL database connection and cursor object to
@@ -14,9 +114,13 @@ mysqlQuery = MysqlQuery(HOST, DATABASE, USER, PASSWORD)
 	2. Define MySQL queries such as INSERT, SELECT, DELETE
 	3. Close database connection""" 
 
-mysqlQuery.connectDatabase()
+# mysqlQuery.connectDatabase()
+# mysqlQuery.queryCmd("insert into device_1(DATA) values('test'")
+# mysqlQuery.createTable("device_2")
 # mysqlQuery.insertTable("mqtt_data", "hello world")
 # mysqlQuery.deleteDataTable()
 # mysqlQuery.readAllTable("mqtt_data")
-mysqlQuery.readRecentTable("mqtt_data", 2)
-mysqlQuery.finishConnect()
+# mysqlQuery.readRecentTable("device_1", 2)
+# for i in range(2):
+# 	mysqlQuery.createTable("device_" + str(i))
+# mysqlQuery.finishConnect()
